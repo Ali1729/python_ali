c69f417c8c5996a49f9c5303984e3a5534adc4f2
Ali <alishareef3@gamil.com>
2023-02-10 19:16:45 -0600

    day2

--- Day2/5_1_list_slicing.py
+++ Day2/5_1_list_slicing.py
@@ -1,1 +1,12 @@
 # Syntax Lst[ Initial : End : IndexJump ]
+
+# Offset Position.
+
+a = list([1,2,34])
+
+for i in a:
+    print(i)
+
+for i in range(10):
+    print(a[i])
+
---
+++ Day2/indentation.py
@@ -0,0 +1,5 @@
+def a():
+	print("asdf")
+	print("awer")
+
+a()
